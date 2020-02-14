36ed1b582e05e9e32050001e8fbea76497f09206
Konrad Abe <kae@dasburo.com>
2020-02-14 16:22:52 +0100

    Added eslint and editorconfig

--- App.js
+++ App.js
@@ -6,20 +6,20 @@ import configureStore from './src/redux/store'
 const store = configureStore()
 
 export default function App() {
-  return (
-    <Provider store={store}>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+            </View>
+        </Provider>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
