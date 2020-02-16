e2ea784b19f8f0f91c0454694d51826d6602bc9f
Konrad Abe <kae@dasburo.com>
2020-02-16 01:15:14 +0100

    Added more scenes and Splash auto navigation

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import SceneSplash from '../scenes/auth/sceneSplash'
+import SceneAppLoading from '../scenes/auth/sceneAppLoading'
+import SceneLogin from '../scenes/auth/sceneLogin'
+import SceneHome from '../scenes/auth/sceneHome'
+import SceneSettings from '../scenes/auth/sceneSettings'
+import SceneGameHome from '../scenes/game/sceneGameHome'
 
 const Stack = createStackNavigator()
 
 const Navigation = () => (
     <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="Splash">
             <Stack.Screen name="Splash" component={SceneSplash} options={{ headerShown: false }} />
+            <Stack.Screen name="AppLoading" component={SceneAppLoading} options={{ headerShown: false }} />
+            <Stack.Screen name="Login" component={SceneLogin} options={{ headerShown: false }} />
+            <Stack.Screen name="Home" component={SceneHome} options={{ headerShown: false }} />
+            <Stack.Screen name="Settings" component={SceneSettings} options={{ headerShown: false }} />
+            <Stack.Screen name="GameHome" component={SceneGameHome} options={{ headerShown: false }} />
         </Stack.Navigator>
     </NavigationContainer>
 )
--- src/scenes/auth/sceneSplash.jsx
+++ src/scenes/auth/sceneSplash.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
+import PropTypes from 'prop-types'
 import DefaultPage from '../../components/DefaultPage'
 
-const SceneSplash = () => (
-    <DefaultPage>
-        <Text>
-            Splash
-        </Text>
-    </DefaultPage>
-)
+const SceneSplash = ({ navigation }) => {
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.navigate('AppLoading')
+        }, 2000)
+    }, [])
+    return (
+        <DefaultPage>
+            <Text>
+                Splash
+            </Text>
+        </DefaultPage>
+    )
+}
+
+SceneSplash.propTypes = {
+    navigation: PropTypes.object.isRequired,
+}
 
 export default SceneSplash
