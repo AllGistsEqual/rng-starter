f810b44f497bf4663ade86b2c1033f268b75b077
Konrad Abe <kae@dasburo.com>
2020-02-17 12:59:22 +0100

    Added Navigation to Home and Settings

--- src/scenes/auth/sceneHome.jsx
+++ src/scenes/auth/sceneHome.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Button } from 'react-native'
+import PropTypes from 'prop-types'
 import DefaultPage from '../../components/DefaultPage'
 
-const SceneHome = () => (
+const SceneHome = ({ navigation }) => (
     <DefaultPage>
         <Text>
             Home
         </Text>
+        <Button title="Settings" onPress={() => navigation.navigate('Settings')} />
+        <Button title="Play the game" onPress={() => navigation.navigate('GameHome')} />
     </DefaultPage>
 )
+
+SceneHome.propTypes = {
+    navigation: PropTypes.object.isRequired,
+}
 
 export default SceneHome
--- src/scenes/auth/sceneSettings.jsx
+++ src/scenes/auth/sceneSettings.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Button, Text } from 'react-native'
+import PropTypes from 'prop-types'
 import DefaultPage from '../../components/DefaultPage'
 
-const SceneSettings = () => (
+const SceneSettings = ({ navigation }) => (
     <DefaultPage>
         <Text>
             Settings
         </Text>
+        <Button title="Back" onPress={() => navigation.goBack()} />
     </DefaultPage>
 )
+
+SceneSettings.propTypes = {
+    navigation: PropTypes.object.isRequired,
+}
 
 export default SceneSettings
