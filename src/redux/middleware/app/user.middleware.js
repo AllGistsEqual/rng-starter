04298615c72a72e2718d2981649ebf554b638a8e
Konrad Abe <kae@dasburo.com>
2020-02-22 19:11:35 +0100

    Fixed evil bug in user middleware

--- src/redux/middleware/app/user.middleware.js
+++ src/redux/middleware/app/user.middleware.js
@@ -4,13 +4,19 @@ import * as RootNavigation from '../../../navigation/RootNavigation'
 const fakeLoginCheck = () => true // It's an older code, sir, but it checks out.
 
 const userMiddleware = () => (next) => (action) => {
-    if (action.type === USER_LOGIN) {
-        if (fakeLoginCheck()) {
-            next(action) // continue with the login
+    switch (action.type) {
+        case USER_LOGIN: {
+            if (fakeLoginCheck()) {
+                next(action) // continue with the login
 
-            setTimeout(() => { // wait a moment before triggering the navigation
-                RootNavigation.navigate('Home')
-            }, 2000)
+                setTimeout(() => { // wait a moment before triggering the navigation
+                    RootNavigation.navigate('Home')
+                }, 2000)
+            }
+            break
+        }
+        default: {
+            next(action)
         }
     }
 }
