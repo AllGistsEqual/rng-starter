fdbd06bf6f7e2d61b60175f41a3994c2df1d724e
Konrad Abe <kae@dasburo.com>
2020-04-09 13:21:17 +0200

    added logout

--- src/redux/middleware/app/user.middleware.js
+++ src/redux/middleware/app/user.middleware.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN } from '../../actions/user.actions'
+import { USER_LOGIN, USER_LOGOUT } from '../../actions/user.actions'
 import * as RootNavigation from '../../../navigation/RootNavigation'
 
 const fakeLoginCheck = () => true // It's an older code, sir, but it checks out.
@@ -13,6 +13,11 @@ const userMiddleware = () => (next) => (action) => {
                     RootNavigation.navigate('Home')
                 }, 2000)
             }
+            break
+        }
+        case USER_LOGOUT: {
+            next(action)
+            RootNavigation.navigate('Login')
             break
         }
         default: {
--- src/scenes/auth/sceneHome.jsx
+++ src/scenes/auth/sceneHome.jsx
@@ -2,19 +2,30 @@ import React from 'react'
 import { Text, Button } from 'react-native'
 import PropTypes from 'prop-types'
 import DefaultPage from '../../components/DefaultPage'
+import {userLogout} from '../../redux/actions/user.actions'
+import {connect} from 'react-redux'
 
-const SceneHome = ({ navigation }) => (
+const SceneHome = ({ logoutUser, navigation }) => (
     <DefaultPage>
         <Text>
             Home
         </Text>
         <Button title="Settings" onPress={() => navigation.navigate('Settings')} />
         <Button title="Play the game" onPress={() => navigation.navigate('GameHome')} />
+        <Button title="Log Out" onPress={() => logoutUser()} />
     </DefaultPage>
 )
 
 SceneHome.propTypes = {
     navigation: PropTypes.object.isRequired,
+    logoutUser: PropTypes.func.isRequired,
 }
 
-export default SceneHome
+const mapDispatchToProps = (dispatch) => ({
+    logoutUser: (name) => dispatch(userLogout({ name })),
+})
+
+export default connect(
+    null,
+    mapDispatchToProps,
+)(SceneHome)
