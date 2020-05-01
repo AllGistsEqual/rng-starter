ad04bcbc26e1197beb4a2ea2467512f0c92ecc7c
Konrad Abe <kae@dasburo.com>
2020-05-01 16:47:46 +0200

    added loading screen and removed status bar

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
-import { enableScreens } from 'react-native-screens';
+import { PersistGate } from 'redux-persist/integration/react'
+import { enableScreens } from 'react-native-screens'
 import store, { persistor } from './src/redux/store'
 import Navigation from './src/navigation'
+import SceneAppLoading from './src/scenes/auth/sceneAppLoading'
 
-enableScreens();
+enableScreens()
 
 export default function App() {
     return (
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
+            <PersistGate loading={<SceneAppLoading />} persistor={persistor}>
+                <StatusBar hidden />
                 <Navigation />
             </PersistGate>
         </Provider>
