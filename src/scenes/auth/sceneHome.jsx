e17d3e726584de3b0bfb13015ad0c121e1dc56f1
Konrad Abe <kae@dasburo.com>
2020-05-01 21:21:09 +0200

    jazzed up sceneHome.jsx with new images

--- src/scenes/auth/sceneHome.jsx
+++ src/scenes/auth/sceneHome.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
-import { Text, Button } from 'react-native'
+import { Image, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { userLogout } from '../../redux/actions/user.actions'
-import DefaultPage from '../../components/global/layout/DefaultPage'
+import TextButton from '../../components/global/ui/TextButton'
+import BackgroundPage from '../../components/global/layout/BackgroundPage'
+import backgroundImage from '../../../assets/bg_abstract_02.jpg'
+import logo from '../../../assets/game_logo.png'
 
 const SceneHome = ({ logout, navigation }) => (
-    <DefaultPage>
-        <Text>
-            Home
-        </Text>
-        <Button title="Settings" onPress={() => navigation.navigate('Settings')} />
-        <Button title="Logout" onPress={logout} />
-        <Button title="Play the game" onPress={() => navigation.navigate('GameHome')} />
-    </DefaultPage>
+    <BackgroundPage background={backgroundImage}>
+        <Image
+            source={logo}
+            style={styles.logo}
+        />
+        <TextButton
+            title="Settings"
+            onPress={() => navigation.navigate('Settings')}
+        />
+        <TextButton
+            title="Logout"
+            onPress={logout}
+        />
+        <TextButton
+            title="Play the game"
+            onPress={() => navigation.navigate('GameHome')}
+        />
+    </BackgroundPage>
 )
+
+const styles = StyleSheet.create({
+    logo: {
+        width: 215,
+        height: 230,
+        marginBottom: 100,
+    },
+})
 
 SceneHome.propTypes = {
     navigation: PropTypes.object.isRequired,
