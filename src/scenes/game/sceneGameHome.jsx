9b76d7db82f2559a45ee5ed9c9e2ef7e5726c795
Konrad Abe <kae@dasburo.com>
2020-05-01 22:40:57 +0200

    made the game pretty

--- src/scenes/game/sceneGameHome.jsx
+++ src/scenes/game/sceneGameHome.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
-import { Text } from 'react-native'
-import DefaultPage from '../../components/global/layout/DefaultPage'
+import { View, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
+import BackgroundPage from '../../components/global/layout/BackgroundPage'
 import RockPaperScissorsLizardSpock from '../../components/other/RockPaperScissorsLizardSpock'
+import IconButton from '../../components/global/ui/IconButton'
+import backgroundImage from '../../../assets/bg_abstract_02.jpg'
 
-const SceneGameHome = () => (
-    <DefaultPage>
-        <Text>
-            Game Home
-        </Text>
+const SceneGameHome = ({ navigation }) => (
+    <BackgroundPage background={backgroundImage}>
+        <View style={styles.contentBox}>
+            <RockPaperScissorsLizardSpock />
 
-        <RockPaperScissorsLizardSpock />
-    </DefaultPage>
+            <IconButton
+                icon="❌"
+                size={30}
+                customStyles={styles.buttonExit}
+                onPress={() => navigation.navigate('Home')}
+            />
+        </View>
+    </BackgroundPage>
 )
+
+const styles = StyleSheet.create({
+    contentBox: {
+        width: '100%',
+        flex: 1,
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonExit: {
+        position: 'absolute',
+        top: 15,
+        right: 15,
+    },
+})
+
+SceneGameHome.propTypes = {
+    navigation: PropTypes.object.isRequired,
+}
 
 export default SceneGameHome
