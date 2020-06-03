7796cdb4ffe69743516dc7fa9974afb5779068a4
Konrad Abe <kae@dasburo.com>
2020-06-03 12:06:52 +0200

    temp dump, added bombs and neighbour counter

--- src/components/other/Cell.jsx
+++ src/components/other/Cell.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import PropTypes from 'prop-types'
 
 const Cell = (props) => {
@@ -11,25 +11,69 @@ const Cell = (props) => {
         height,
     } = props
 
+    let cellContent = ''
+    let cellStyle = ''
+
+    switch (cell) {
+        case (10): {
+            cellContent = '💣'
+            cellStyle = 'bomb'
+            break
+        }
+
+        case (0): {
+            cellContent = ''
+            cellStyle = 'empty'
+            break
+        }
+
+        default: {
+            cellContent = cell
+            cellStyle = 'counter'
+        }
+    }
+
     return (
         <View
             style={{
-                position: 'absolute',
-                borderWidth: 1,
-                borderColor: '#999',
-                justifyContent: 'center',
-                alignItems: 'center',
+                ...styles.cellDefault,
                 width,
                 height,
                 left: width * x,
                 top: height * y,
-                backgroundColor: cell ? '#666' : '#ccc',
+                ...styles[`${cellStyle}Background`],
             }}
         >
-            <Text>{cell}</Text>
+            <Text style={styles[`${cellStyle}Text`]}>
+                {cellContent}
+            </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    cellDefault: {
+        position: 'absolute',
+        borderWidth: 1,
+        borderColor: '#999',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    emptyText: {},
+    emptyBackground: {
+        backgroundColor: '#ddd',
+    },
+    counterText: {
+        fontSize: 18,
+    },
+    counterBackground: {
+        backgroundColor: '#bbb',
+    },
+    bombText: {},
+    bombBackground: {
+        backgroundColor: 'red',
+    },
+})
 
 Cell.propTypes = {
     cell: PropTypes.number.isRequired,
--- src/components/other/GridTest.jsx
+++ src/components/other/GridTest.jsx
@@ -4,31 +4,93 @@ import Cell from './Cell'
 
 const gridPainter = (grid) => grid
     .map(
-        (row, rowIndex) => row.map(
-            (cell, columnIndex) => ({ cell, x: columnIndex, y: rowIndex })
+        (col, colIndex) => col.map(
+            (cell, rowIndex) => ({ cell, x: colIndex, y: rowIndex })
         )
     )
     .flat()
 
 const cellWidth = 50
 const cellHeight = 50
-const gridRows = 10
 const gridCols = 7
+const gridRows = 10
+const gridBombs = 9
 const borderWidth = 1
 
-const createBoard = (rows, cols) => {
-    const tempGrid = Array(rows).fill(Array(cols).fill(0))
+const createBoard = (cols, rows) => {
+    const board = []
+    for (let col = 0; col < cols; col++) {
+        board.push(Array(rows).fill(0))
+    }
+    return board
+}
 
-    return tempGrid
+const copyGrid = (grid) => grid.map((col) => [...col])
+
+const getValueOfCell = (grid, x, y) => grid[x][y]
+
+const getRandomCell = (colLength, rowLength) => ([
+    Math.floor(Math.random() * Math.floor(colLength)),
+    Math.floor(Math.random() * Math.floor(rowLength)),
+])
+
+const addBombs = (grid, cols, rows, bombs) => {
+    const newGrid = copyGrid(grid)
+    const safeBombCount = cols * rows - 5
+    const targetBombCount = Math.min(safeBombCount, bombs)
+
+    let bombCount = 0
+    do {
+        const [x, y] = getRandomCell(cols, rows)
+        if (getValueOfCell(newGrid, x, y) === 0) {
+            newGrid[x][y] = 10
+            bombCount += 1
+        }
+    } while (bombCount < targetBombCount)
+    return newGrid
+}
+
+const oneUpNeighbours = (grid, x, y) => {
+    const newGrid = copyGrid(grid)
+    const neighbours = [
+        [x - 1, y - 1],
+        [x + 0, y - 1],
+        [x + 1, y - 1],
+        [x - 1, y],
+        [x + 1, y],
+        [x - 1, y + 1],
+        [x + 0, y + 1],
+        [x + 1, y + 1],
+    ]
+    neighbours.forEach(([nX, nY]) => {
+        if (newGrid[nX]) {
+            if (newGrid[nX][nY] !== undefined) {
+                if (newGrid[nX][nY] !== 10) {
+                    newGrid[nX][nY] += 1
+                }
+            }
+        }
+    })
+    return newGrid
+}
+
+const addNeighbourCount = (grid) => {
+    let newGrid = copyGrid(grid)
+    newGrid.forEach((col, colIndex) => col.forEach((cell, rowIndex) => {
+        if (cell === 10) { newGrid = oneUpNeighbours(newGrid, colIndex, rowIndex) }
+    }))
+    return newGrid
 }
 
 const GridTest = () => {
-    const grid = createBoard(gridRows, gridCols, 1)
+    const grid = createBoard(gridCols, gridRows)
+    const riggedGrid = addBombs(grid, gridCols, gridRows, gridBombs)
+    const countedGrid = addNeighbourCount(riggedGrid)
 
     return (
         <>
             <View style={styles.gridBackground}>
-                {gridPainter(grid).map(({
+                {gridPainter(countedGrid).map(({
                     cell,
                     x,
                     y,
