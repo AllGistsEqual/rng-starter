ed481aa0e5428a5f948ab52fe050b50b435aec5b
Konrad Abe <43298587+AllBitsEqual@users.noreply.github.com>
2022-06-24 13:42:00 +0200

    feat: added redux persist, asyncStorage
    upped react types to 17.0.44 to prevent a conflict in types and PersistGate

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
 import { Provider } from 'react-redux'
-import store from './src/redux'
+import { PersistGate } from 'redux-persist/integration/react';
+import store, {persistor} from './src/redux'
 import Demo from './src/screens/Demo'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Demo />
-      <StatusBar style="auto" />
+      <PersistGate loading={null} persistor={persistor}>
+        <Demo />
+        <StatusBar style="auto" />
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,16 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    persistReducer,
+    persistStore,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import rootReducer from './rootReducer'
 import middleware from './rootMiddleware'
+
+// blacklist a store attribute using it's reducer name. Blacklisted attributes will not persist.
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: AsyncStorage,
+    blacklist: [], //blacklisting a store attribute name, will not persist that store attribute.
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // We do not want to maintain the return type of createStore function by hand
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const createStore = () =>
     configureStore({
-        reducer: rootReducer,
+        reducer: persistedReducer,
         middleware: (getDefaultMiddleware) =>
             getDefaultMiddleware({
-                serializableCheck: false, // ToDo: refactor to have an serializable state !!!
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+                }
             }).concat(middleware),
     })
 const store = createStore()
@@ -22,4 +45,5 @@ export type AppDispatch = typeof store.dispatch
 export const useReduxDispatch = (): AppDispatch => useDispatch<AppDispatch>()
 export const useReduxSelector: TypedUseSelectorHook<RootState> = useSelector
 
+export const persistor = persistStore(store);
 export default store
